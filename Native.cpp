7f7dc85cd2adada88cc7c5bdf4bb1f5744a936da
Solomka <76917656+Ingur-5967@users.noreply.github.com>
2024-10-02 23:15:42 +0300

    Add files via upload
    Practive-Homework-4

---
+++ Native.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	float X = 0.0, Y = 0.0;
+	cout << "Введите значения x и y\n";
+	cin >> X;
+	cin >> Y;
+
+	float R = (-X + sqrt(abs(pow(X, 2) - (4 * X * Y)))) / (2 * X);
+	float S = pow(4, (- 3 * X)) * sin(Y);
+
+	cout << R << endl << S << endl;
+
+	float C = max(R, S);
+
+	cout << C << endl;
+
+	system("pause");
+
+	return 0;
+}
